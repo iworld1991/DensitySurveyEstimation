5e9ac8c560872475dba5cd3312245dd831a06e93
iworld1991 <twang80@jhu.edu>
2021-12-01 21:46:21 -0500

    1.add skewness and kurtosis as moments. 2. replace q90/q10 with q90-q10. 3. simply the sample data. with only one variable and 10 bins.

--- python/DensityEst.py
+++ python/DensityEst.py
@@ -13,13 +13,13 @@
 #     name: python3
 # ---
 
-# ### Density Estimation 
+# ### Parametric Estimation of Bin-based Density Forecast in Surveys 
 #
 # - Following [Manski et al.(2009)](https://www.tandfonline.com/doi/abs/10.1198/jbes.2009.0003)
 # - Three cases 
 #    - case 1. 3+ intervales with positive probabilities, to be fitted with a generalized beta distribution
 #    - case 2. exactly 2 adjacent intervals with positive probabilities, to be fitted with a triangle distribution 
-#    - case 3. __one or multiple__ intervals with equal probabilities, to be fitted with a uniform distribution
+#    - case 3. __one or multiple__ adjacent intervals with equal probabilities, to be fitted with a uniform distribution
 #    
 
 from scipy.stats import gamma
@@ -41,7 +41,7 @@ def quantile(x,quantiles):
 
 # ### Case 1. Generalized Beta Distribution
 
-# + code_folding=[]
+# + code_folding=[0]
 def GeneralizedBetaEst(bin,probs):
     """
     This fits a histogram with positive probabilities in at least 3 bins to a generalized beta distribution.
@@ -109,7 +109,7 @@ def GeneralizedBetaEst(bin,probs):
         return para_est   # could be 2 or 4 parameters 
 
 
-# + code_folding=[]
+# + code_folding=[0]
 def GeneralizedBetaStats(a,b,lb,ub):
     """
     This function computes the moments of a generalized beta distribution, mean and variance for now. 
@@ -127,14 +127,24 @@ def GeneralizedBetaStats(a,b,lb,ub):
            ratio 90 10, float
     """
     # lb=0 and ub=1 for a standard beta distribution
-    mean = lb + (ub-lb)*a/(a+b)
-    var = (ub-lb)**2*a*b/((a+b)**2*(a+b+1))
+    #mean = lb + (ub-lb)*a/(a+b)
+    #var = (ub-lb)**2*a*b/((a+b)**2*(a+b+1))
+    mean, var, skew, kurt = beta.stats(a, 
+                                       b, 
+                                       loc=lb, 
+                                       scale=ub-lb, 
+                                       moments='mvsk')
     std = np.sqrt(var)
     # ratio
     x = np.linspace(beta.ppf(0.01, a, b), beta.ppf(0.99, a, b), 100)
     quantiles = quantile(x,[0.1, 0.9])
-    iqr1090 = quantiles[0]/quantiles[1]
-    return {"mean": mean,"variance":var, "std": std, "iqr1090": iqr1090}
+    iqr1090 = quantiles[0]-quantiles[1]
+    return {"mean": mean,
+            "variance":var, 
+            "std": std, 
+            "iqr1090": iqr1090,
+            "skewness":skew,
+            "kurtosis":kurt}
 
 
 # -
@@ -164,7 +174,7 @@ def GeneralizedBetaStats(a,b,lb,ub):
 # $$ \implies c = (t - b) - \frac{2}{h} $$
 # $$ \implies c = ub - \frac{2}{h} $$
 
-# + code_folding=[]
+# + code_folding=[0]
 def TriangleEst(bin,probs):
     """
     The function fits histograms with exactly two adjacent 
@@ -224,10 +234,10 @@ def TriangleEst(bin,probs):
                 h = 2/(ub-lb)
                 c = ub - 2/h
         else:
-            lb = []
-            ub = []
-            h = []
-            c = []
+            lb = np.nan
+            ub = np.nan
+            h = np.nan
+            c = np.nan
             print('Warning: the two intervals are not adjacent or are open-ended')
     return {'lb':lb,'ub':ub,"height":h,"mode":c}
 
@@ -248,7 +258,7 @@ def TriangleEst(bin,probs):
 #
 #
 
-# + code_folding=[]
+# + code_folding=[0]
 def TriangleStats(lb,ub,c):
     """
     parameters
@@ -268,51 +278,24 @@ def TriangleStats(lb,ub,c):
     mean = (lb+ub)/2
     var = (lb**2+ub**2+(lb+ub)**2/4-lb*(lb+ub)/2-ub*(lb+ub)/2-lb*ub)/18
     std = np.sqrt(var)
+    skew = 0
+    kurt = -3/5
     x = np.linspace(triang.ppf(0.01, c, lb, ub), triang.ppf(0.99, c, lb, ub), 100)
     quantiles = quantile(x,[0.1, 0.9])
-    iqr1090 = quantiles[0]/quantiles[1]
-    return {"mean": mean,"variance":var, "std": std, "iqr1090": iqr1090}
+    iqr1090 = quantiles[0]-quantiles[1]
+    return {"mean": mean,
+            "variance":var, 
+            "std": std, 
+            "iqr1090": iqr1090,
+           'skewness':skew,
+            'kurtosis':kurt}
 
 
 # -
 
 # ### Case 3. Uniform Distribution
 
-# + code_folding=[]
-def UniformEst1bin(bin,probs):
-    """
-    This function fits a histogram with only one bin of positive probability to a uniform distribution.
-    
-    paramters
-    ---------
-    bin:  ndarray, (n+1) x 1 
-          positions for n bins in the histograms 
-          
-    probs:  ndarrray n x 1
-          each entry is a probability for each of the n bins given by the surveyee, between 0 and 1
-    
-    returns
-    --------
-    dict: 2 keys
-          lb and ub, float. the left and right bounds of the uniform distribution
-    """
-    pprob=[i for i in range(len(bin)-1) if probs[i]>0]
-    if len(pprob)==1:
-        if pprob[0]!=0 and pprob[0]!=len(bin)-1:
-            lb = bin[pprob[0]]
-            ub = bin[pprob[0]+1]
-        else:
-            lb=[]
-            ub=[]
-    else:
-        lb=[]
-        ub=[]
-    return {"lb":lb,"ub":ub}
-
-
-# -
-
-# New function: Takes into account that many bins have the same probability
+# + code_folding=[0]
 def UniformEst(bins,probs):
     """
     This function fits a histogram with only one bin of positive probability to a uniform distribution.
@@ -356,13 +339,13 @@ def UniformEst(bins,probs):
         ub = bins[pos_entry[0] + len(pos_bins)] 
         ub = ub[0] # into float
     if len(pos_bins)==0:
-        lb=[]
-        ub=[]
+        lb=np.nan
+        ub=np.nan
 
     return {"lb":lb,"ub":ub}
 
 
-# + code_folding=[]
+# + code_folding=[0]
 def UniformStats(lb,ub):
     """
     The function computes the moment of a uniform distribution.
@@ -380,20 +363,29 @@ def UniformStats(lb,ub):
            irq1090, float  
     """
     
-    if lb and ub: ## empty list is True
+    if lb is not np.nan and ub is not np.nan: 
         mean = (lb+ub)/2
         var = (ub-lb)**2/12
         std = np.sqrt(var)
         x = np.linspace(uniform.ppf(lb),uniform.ppf(ub), 100)
         p10 = np.percentile(x, 10)
         p90 = np.percentile(x, 90)
-        iqr1090 = p90/p10
+        iqr1090 = p90-p10
+        skew = 0
+        kurt = -5/6
     else:
-        mean=[]
-        var=[]
-        std = []
-        iqr1090 = []
-    return {"mean": mean,"variance":var, "std": std, "iqr1090": iqr1090}
+        mean=np.nan
+        var=np.nan
+        std = np.nan
+        iqr1090 = np.nan
+        skew = np.nan
+        kurt = np.nan
+    return {"mean": mean,
+            "variance":var, 
+            "std": std, 
+            "iqr1090": iqr1090,
+            "skewness":skew,
+            "kurtosis":kurt}
 
 
 # -
@@ -403,14 +395,23 @@ def UniformStats(lb,ub):
 # + code_folding=[]
 ## test 1: GenBeta Dist
 sim_bins= np.array([0,0.2,0.32,0.5,1,1.2])
-sim_probs=np.array([0,0.2,0.5,0.3,0])
-GeneralizedBetaEst(sim_bins,sim_probs)
+sim_probs=np.array([0,0.2,0.3,0.1,0.4])
+para_est = GeneralizedBetaEst(sim_bins,sim_probs)
+print(para_est)
+GeneralizedBetaStats(para_est[0],
+                    para_est[1],
+                    para_est[2],
+                    para_est[3])
 
-# + code_folding=[0]
+# + code_folding=[]
 ## test 2: Triangle Dist
 sim_bins2 = np.array([0,0.2,0.32,0.5,1,1.2])
 sim_probs2=np.array([0,0.2,0.8,0,0])
-TriangleEst(sim_bins2,sim_probs2)
+para_est = TriangleEst(sim_bins2,sim_probs2)
+print(para_est)
+TriangleStats(para_est['lb'],
+              para_est['ub'], 
+              para_est['mode'])
 
 # + code_folding=[]
 ## test 3: Uniform Dist with one interval
@@ -419,39 +420,49 @@ sim_probs3 = np.array([0,0,1,0,0])
 para_est= UniformEst(sim_bins3,sim_probs3)
 print(para_est)
 UniformStats(para_est['lb'],para_est['ub']) 
+
+# + code_folding=[]
+## test 4: Uniform Dist with multiple adjacent bins with same probabilities 
+sim_bins4 = np.array([0,0.2,0.32,0.5,1,1.2])
+sim_probs4=np.array([0.2,0.2,0.2,0.2,0.2])
+para_est= UniformEst(sim_bins4,
+                     sim_probs4)
+print(para_est)
+UniformStats(para_est['lb'],
+             para_est['ub']) 
+
+
+# + code_folding=[]
+## test 5: Uniform Dist with multiple non-adjacent bins with equal probabilities
+sim_bins5 = np.array([0,0.2,0.32,0.5,1,1.2])
+sim_probs5=np.array([0,0.5,0,0.5,0])
+para_est = UniformEst(sim_bins5,
+                     sim_probs5)
+print(para_est)
+UniformStats(para_est['lb'],
+             para_est['ub']) 
 # -
 
-## test 4: Uniform Dist with multiple adjacent bins with same probabilities 
-sim_bins3 = np.array([0,0.2,0.32,0.5,1,1.2])
-sim_probs3=np.array([0.2,0.2,0.2,0.2,0.2])
-para_est= UniformEst(sim_bins3,sim_probs3)
-print(para_est)
-UniformStats(para_est['lb'],para_est['ub']) 
-
-
-## test 5: Uniform Dist with multiple non-adjacent bins with equal probabilities
-sim_bins3 = np.array([0,0.2,0.32,0.5,1,1.2])
-sim_probs3=np.array([0,0.5,0,0.5,0])
-para_est= UniformEst(sim_bins3,sim_probs3)
-print(para_est)
-UniformStats(para_est['lb'],para_est['ub']) 
+UniformStats(para_est['lb'],
+             para_est['ub'])
 
 # ### Test with simulated data from known distribution 
 # - we simulate data from a true beta distribution with known parameters
 # - then we estimate the parameters with our module and see how close it is with the true parameters 
 
-# + code_folding=[]
+# + code_folding=[0]
 ## simulate a generalized distribution
 sim_n=1000
-true_alpha,true_beta,true_loc,true_scale=1.4,2.2,0,3
+true_alpha,true_beta,true_loc,true_scale=0.4,0.8,0,1
 sim_data = beta.rvs(true_alpha,true_beta,
                     loc=true_loc,
                     scale=true_scale,
                     size=sim_n)
 sim_bins2=plt.hist(sim_data)[1]
 sim_probs2=plt.hist(sim_data)[0]/sim_n
-sim_est=GeneralizedBetaEst(sim_bins2,sim_probs2)
 
+sim_est=GeneralizedBetaEst(sim_bins2,
+                           sim_probs2)
 print('Estimated parameters',sim_est)
 
 print('Estimated moments:',GeneralizedBetaStats(sim_est[0],
@@ -474,7 +485,7 @@ plt.hist(sim_data,density=True,label='Dist of Simulated Data')
 plt.legend(loc=0)
 
 
-# + code_folding=[]
+# + code_folding=[1]
 ## This is the synthesized density estimation function
 def SynDensityStat(bin,probs):
     """
@@ -551,9 +562,20 @@ def SynDensityStat(bin,probs):
             print(para_est)
             return UniformStats(para_est['lb'],para_est['ub'])            
         else:
-            return {"mean":[],"variance":[], "std":[], "iqr1090":[]}
+            return {"mean":None,
+                    'std': None,
+                    'iqr1090':None,
+                     "variance":None,
+                    "variance":None,
+                    "skewness":None,
+                    "kurtosis":None}
     else:
-        return {"mean":[],"variance":[], "std":[], "iqr1090":[]}
+        return {"mean":np.nan,
+                'std': np.nan,
+                'iqr1090':np.nan,
+                "variance":np.nan,
+                "skewness":np.nan,
+                "kurtosis":np.nan}
 
 # + code_folding=[]
 ## testing the synthesized estimator function using an arbitrary example created above
@@ -563,9 +585,10 @@ SynDensityStat(sim_bins2,sim_probs2)['iqr1090']
 # + code_folding=[]
 ### loading probabilistic data  
 IndSPF=pd.read_stata('../data/sample_data.dta')   
-# SPF inflation quarterly 
-# 2 Inf measures: CPI and PCE
-# 2 horizons: y-1 to y  and y to y+1
+## this is the quarterly SPF forecast on 1-year ahead core inflation 
+# -
+
+IndSPF
 
 # + code_folding=[]
 ## survey-specific parameters 
@@ -573,28 +596,20 @@ nobs=len(IndSPF)
 SPF_bins=np.array([-10,0,0.5,1,1.5,2,2.5,3,3.5,4,10])
 print("There are "+str(len(SPF_bins)-1)+" bins in SPF")
 
-# + code_folding=[]
+# + code_folding=[0]
 ##############################################
 ### attention: the estimation happens here!!!!!
 ###################################################
 
-#IndSPF['PRCCPIMean0']='nan'   # CPI from y-1 to y 
-#IndSPF['PRCCPIVar0']='nan'    
-#IndSPF['PRCCPIMean1']='nan'  # CPI from y to y+1  
-#IndSPF['PRCCPIVar1']='nan'
-#IndSPF['PRCPCEMean0']='nan' # PCE from y-1 to y
-#IndSPF['PRCPCEVar0']='nan'
-#IndSPF['PRCPCEMean1']='nan' # PCE from y to y+1
-#IndSPF['PRCPCEVar1']='nan'
-
 
 ## creating positions 
 index  = IndSPF.index
-columns=['PRCCPIMean0','PRCCPIVar0', 'PRCCPIStd0','PRCCPIIqr10900',
-         'PRCCPIMean1','PRCCPIVar1','PRCCPIStd1','PRCCPIIqr10901',
-         'PRCPCEMean0','PRCPCEVar0','PRCPCEStd0','PRCPCEIqr10900',
-         'PRCPCEMean1','PRCPCEVar1','PRCPCEStd1','PRCPCEIqr10901']
-IndSPF_moment_est = pd.DataFrame(index=index,columns=columns)
+columns=['Mean',
+         'Var', 
+         'Std',
+         'Iqr1090']
+IndSPF_moment_est = pd.DataFrame(index=index,
+                                 columns=columns)
 
 ## Invoking the estimation
 
@@ -603,57 +618,17 @@ for i in range(nobs):
     ## take the probabilities (flip to the right order, normalized to 0-1)
     PRCCPI_y0 = np.flip(np.array([IndSPF.iloc[i,:]['PRCCPI'+str(n)]/100 for n in range(1,11)]))
     print(PRCCPI_y0)
-    PRCCPI_y1 = np.flip(np.array([IndSPF.iloc[i,:]['PRCCPI'+str(n)]/100 for n in range(11,21)]))
-    print(PRCCPI_y1)
-    PRCPCE_y0 = np.flip(np.array([IndSPF.iloc[i,:]['PRCPCE'+str(n)]/100 for n in range(1,11)]))
-    print(PRCCPI_y0)
-    PRCPCE_y1 = np.flip(np.array([IndSPF.iloc[i,:]['PRCPCE'+str(n)]/100 for n in range(11,21)]))
-    print(PRCCPI_y1)
     if not np.isnan(PRCCPI_y0).any():
         stats_est=SynDensityStat(SPF_bins,PRCCPI_y0)
         if len(stats_est)>0:
-            IndSPF_moment_est['PRCCPIMean0'][i]=stats_est['mean']
+            IndSPF_moment_est['Mean'][i]=stats_est['mean']
             print(stats_est['mean'])
-            IndSPF_moment_est['PRCCPIVar0'][i]=stats_est['variance']
+            IndSPF_moment_est['Var'][i]=stats_est['variance']
             print(stats_est['variance'])
-            IndSPF_moment_est['PRCCPIStd0'][i]=stats_est['std']
+            IndSPF_moment_est['Std'][i]=stats_est['std']
             print(stats_est['std'])
-            IndSPF_moment_est['PRCCPIIqr10900'][i]=stats_est['iqr1090']
-            print(stats_est['iqr1090'])            
-    if not np.isnan(PRCCPI_y1).any():
-        stats_est=SynDensityStat(SPF_bins,PRCCPI_y1)
-        if len(stats_est):
-            print(stats_est['mean'])
-            IndSPF_moment_est['PRCCPIMean1'][i]=stats_est['mean']
-            print(stats_est['variance'])
-            IndSPF_moment_est['PRCCPIVar1'][i]=stats_est['variance']
-            print(stats_est['std'])
-            IndSPF_moment_est['PRCCPIStd1'][i]=stats_est['std']
-            print(stats_est['iqr1090'])
-            IndSPF_moment_est['PRCCPIIqr10901'][i]=stats_est['iqr1090']
-    if not np.isnan(PRCPCE_y0).any():
-        if len(stats_est)>0:
-            stats_est=SynDensityStat(SPF_bins,PRCPCE_y0)
-            print(stats_est['mean'])
-            IndSPF_moment_est['PRCPCEMean0'][i]=stats_est['mean']
-            print(stats_est['variance'])
-            IndSPF_moment_est['PRCPCEVar0'][i]=stats_est['variance']
-            print(stats_est['std'])
-            IndSPF_moment_est['PRCPCEStd0'][i]=stats_est['std']
-            print(stats_est['iqr1090'])
-            IndSPF_moment_est['PRCPCEIqr10900'][i]=stats_est['iqr1090']
-    if not np.isnan(PRCPCE_y1).any():
-        if len(stats_est)>0:
-            stats_est=SynDensityStat(SPF_bins,PRCPCE_y1)
-            print(stats_est)
-            print(stats_est['mean'])
-            IndSPF_moment_est['PRCPCEMean1'][i]=stats_est['mean']
-            print(stats_est['variance'])
-            IndSPF_moment_est['PRCPCEVar1'][i]=stats_est['variance']
-            print(stats_est['std'])
-            IndSPF_moment_est['PRCPCEStd1'][i]=stats_est['std']
-            print(stats_est['iqr1090'])
-            IndSPF_moment_est['PRCPCEIqr10900'][i]=stats_est['iqr1090']
+            IndSPF_moment_est['Iqr1090'][i]=stats_est['iqr1090']
+            print(stats_est['iqr1090'])           
 # -
 
 ### exporting moments estimates to pkl
@@ -661,49 +636,30 @@ IndSPF_est = pd.concat([IndSPF,IndSPF_moment_est], join='inner', axis=1)
 IndSPF_est.to_pickle("./DstSampleEst.pkl")
 IndSPF_pk = pd.read_pickle('./DstSampleEst.pkl')
 
-# +
-IndSPF_pk['PRCCPIMean0']=pd.to_numeric(IndSPF_pk['PRCCPIMean0'],errors='coerce')   # CPI from y-1 to y 
-IndSPF_pk['PRCCPIVar0']=pd.to_numeric(IndSPF_pk['PRCCPIVar0'],errors='coerce')  
-IndSPF_pk['PRCCPIStd0']=pd.to_numeric(IndSPF_pk['PRCCPIStd0'],errors='coerce') 
-IndSPF_pk['PRCCPIIqr10900']=pd.to_numeric(IndSPF_pk['PRCCPIIqr10900'],errors='coerce') 
+IndSPF_pk['Mean']=pd.to_numeric(IndSPF_pk['Mean'],errors='coerce')   # CPI from y-1 to y 
+IndSPF_pk['Var']=pd.to_numeric(IndSPF_pk['Var'],errors='coerce')  
+IndSPF_pk['Std']=pd.to_numeric(IndSPF_pk['Std'],errors='coerce') 
+IndSPF_pk['Iqr1090']=pd.to_numeric(IndSPF_pk['Iqr1090'],errors='coerce') 
 
-IndSPF_pk['PRCCPIMean1']=pd.to_numeric(IndSPF_pk['PRCCPIMean1'],errors='coerce')   # CPI from y to y+1  
-IndSPF_pk['PRCCPIVar1']=pd.to_numeric(IndSPF_pk['PRCCPIVar1'],errors='coerce')
-IndSPF_pk['PRCCPIStd1']=pd.to_numeric(IndSPF_pk['PRCCPIStd1'],errors='coerce')
-IndSPF_pk['PRCCPIIqr10901']=pd.to_numeric(IndSPF_pk['PRCCPIIqr10901'],errors='coerce')
-
-IndSPF_pk['PRCPCEMean0']=pd.to_numeric(IndSPF_pk['PRCPCEMean0'],errors='coerce')  # PCE from y-1 to y
-IndSPF_pk['PRCPCEVar0']=pd.to_numeric(IndSPF_pk['PRCPCEVar0'],errors='coerce') 
-IndSPF_pk['PRCPCEStd0']=pd.to_numeric(IndSPF_pk['PRCPCEStd0'],errors='coerce') 
-IndSPF_pk['PRCPCEIqr10900']=pd.to_numeric(IndSPF_pk['PRCPCEIqr10900'],errors='coerce') 
-
-IndSPF_pk['PRCPCEMean1']=pd.to_numeric(IndSPF_pk['PRCPCEMean1'],errors='coerce')  # PCE from y to y+1
-IndSPF_pk['PRCPCEVar1']=pd.to_numeric(IndSPF_pk['PRCPCEVar1'],errors='coerce')
-IndSPF_pk['PRCPCEStd1']=pd.to_numeric(IndSPF_pk['PRCPCEStd1'],errors='coerce')
-IndSPF_pk['PRCPCEIqr10901']=pd.to_numeric(IndSPF_pk['PRCPCEIqr10901'],errors='coerce')
-# -
 
 IndSPF_pk.tail()
 
-IndSPF_pk.to_stata('../data/Dstsample_data.dta')
+IndSPF_pk.to_excel('../data/Dstsample_data.xlsx')
 
 # + code_folding=[]
 ### Robustness checks: focus on big negative mean estimates 
 sim_bins_data = SPF_bins
-print(str(sum(IndSPF_pk['PRCCPIMean1']<=0))+' abnormals')
+print(str(sum(IndSPF_pk['Mean']<=0))+' abnormals')
 ct=0
 figure=plt.plot()
-for id in IndSPF_pk.index[IndSPF_pk['PRCCPIMean1']<-2]:
+for id in IndSPF_pk.index[IndSPF_pk['Mean']<-2]:
     print(id)
-    print(IndSPF_pk['PRCCPIMean1'][id])
-    sim_probs_data= np.flip(np.array([IndSPF['PRCCPI'+str(n)][id]/100 for n in range(11,21)]))
+    print(IndSPF_pk['Mean'][id])
+    sim_probs_data= np.flip(np.array([IndSPF['PRCCPI'+str(n)][id]/100 for n in range(1,11)]))
     plt.bar(sim_bins_data[1:],sim_probs_data)
     print(sim_probs_data)
-    stats_est=SynDensityStat(SPF_bins,sim_probs_data)
+    stats_est=SynDensityStat(SPF_bins,
+                             sim_probs_data)
     print(stats_est['mean'])
 # -
-
-
-
-
 
